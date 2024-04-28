0ef437b8511531dc0121e28b4912a58f09bfa071
Juunini <juuni.ni.i@gmail.com>
2024-04-28 19:32:34 +0900

    add ResearchButton component

---
+++ src/lib/components/ResearchButton/ResearchButton.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	export let title: string;
+	export let src: string;
+</script>
+
+<article>
+	<div class="card">
+		<h4 class="title">{title}</h4>
+		<div class="body">
+			<img {src} alt="" />
+		</div>
+		<div class="footer" />
+	</div>
+</article>
+
+<style>
+	article {
+		display: block;
+		width: 11.625rem;
+		background: linear-gradient(-30deg, #080807, #898888);
+		padding: 0.0625rem 0.1875rem 0 0.1875rem;
+		border-radius: 0.25rem;
+		overflow: hidden;
+	}
+
+	.card {
+		position: relative;
+		border: 0.0625rem solid #000;
+		border-radius: 0.25rem;
+		overflow: hidden;
+	}
+
+	.card::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		box-shadow:
+			0.0625rem 0.0625rem 0 0 rgba(255, 255, 255, 0.2) inset,
+			-0.0625rem -0.0625rem 0 0 rgba(0, 0, 0, 0.2) inset;
+	}
+
+	.title {
+		height: 0.875rem;
+		color: #fff;
+		padding-top: 0.1875rem;
+		padding-left: 0.3125rem;
+		font-size: 0.8125rem;
+		font-weight: 400;
+		letter-spacing: 0.0625rem;
+		text-shadow:
+			-0.0625rem 0 #000,
+			0 0.0625rem #000,
+			0.0625rem 0 #000,
+			0 -0.0625rem #000;
+		background: linear-gradient(-30deg, #776a48, #d6cdae);
+		border-bottom: 0.0625rem solid rgba(211, 194, 147, 0.1);
+	}
+
+	.body {
+		height: 2.9375rem;
+		background: linear-gradient(-30deg, #453d2a, #c8b889);
+	}
+
+	.body img {
+		margin-top: -0.5rem;
+		margin-left: -1rem;
+		filter: drop-shadow(0 0 0 rgba(0, 0, 0, 0.4));
+	}
+
+	.footer {
+		height: 0.875rem;
+		background: linear-gradient(-30deg, #2c271a, #5d5239);
+		border-top: 0.0625rem solid rgba(60, 53, 36, 0.1);
+	}
+</style>
---
+++ src/lib/components/ResearchButton/ReserchButton.stories.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import ResearchButton from './ResearchButton.svelte';
+
+	export const meta = {
+		title: 'components/ResearchButton',
+		component: ResearchButton,
+		tags: ['autodocs']
+	};
+</script>
+
+<Template let:args>
+	<ResearchButton {...args} />
+</Template>
+
+<Story
+	name="Default"
+	args={{
+		title: '1934년형 경전차',
+		src: '/assets/tanks/Generic_light_tank_chassis_1.png'
+	}}
+/>
